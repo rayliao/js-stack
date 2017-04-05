3a5d237d6004186344bd3c71fab80a0f21c4f21b
laoyl <liaoyl@mycode.best>
2017-04-05 16:28:06 +0800

    route

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,32 +1,42 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import Immutable from 'immutable'
 
-import App from './app'
-import helloReducer from './reducer/hello'
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
-const store = createStore(combineReducers({ hello: helloReducer }),
-    isProd ? undefined : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composeEnhancers = (isProd ? undefined : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+
+const store = createStore(combineReducers(
+    { hello: helloReducer }),
+    { hello: Immutable.fromJS(preloadedState.hello) },
+    composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
 const wrapApp = (AppComponent, reduxStore) =>
     <Provider store={reduxStore}>
-        <AppContainer>
-            <AppComponent />
-        </AppContainer>
+        <BrowserRouter>
+            <AppContainer>
+                <AppComponent />
+            </AppContainer>
+        </BrowserRouter>
     </Provider>
 
 ReactDOM.render(wrapApp(App, store), rootEl)
 
 if (module.hot) {
-    module.hot.accept('./app', () => {
-        const NextApp = require('./app').default
+    module.hot.accept('../shared/app', () => {
+        const NextApp = require('../shared/app').default
         ReactDOM.render(wrapApp(NextApp, store), rootEl)
     })
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,9 @@
 import compression from 'compression'
 import express from 'express'
 
-import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config'
+import { STATIC_PATH, WEB_PORT } from '../shared/config'
 import { isProd } from '../shared/util'
-import renderApp from './render-app'
-
-import { helloEndpointRoute } from '../shared/routes'
+import routing from './routing'
 
 const app = express()
 
@@ -13,13 +11,7 @@ app.use(compression())
 app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send(renderApp(APP_NAME))
-})
-
-app.get(helloEndpointRoute(), (req, res) => {
-    res.json({ serverMessage: `Hello from the server! (received ${req.params.num})`})
-})
+routing(app)
 
 app.listen(WEB_PORT, () => {
     console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development). \n Keep "yarn dev:wds" running in an other terminal'}.`)
